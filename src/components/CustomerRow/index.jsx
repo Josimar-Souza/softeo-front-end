69f006bba6b79e947fa7decf1c2a39cf6814073c
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-03 15:01:49 -0300

    Created method for customer details button

--- src/components/CustomerRow/index.jsx
+++ src/components/CustomerRow/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './customerRowStyles';
 import getCurrencyFormat from '../../helpers/getCurrencyFormat';
 import Button from '../Button';
 
 function CustomerRow({ customer }) {
+  const navigate = useNavigate();
+
   const getInstallmentsTotal = (installments) => {
     const total = installments.reduce((acc, curr) => acc + curr.value, 0);
     const formattedTotal = getCurrencyFormat(total, 'pt-BR', 'currency', 'BRL');
@@ -18,18 +21,23 @@ function CustomerRow({ customer }) {
     CustomerActions,
   } = styles;
 
+  const onDetailsButtonClick = () => {
+    navigate(`/${customer._id}`);
+  };
+
   const removeButtonConfig = {
     backgroundColor: 'red',
     fontColor: 'white',
     fontSize: '1.2vw',
-    width: '45%',
+    width: '47%',
   };
 
   const detailsButtonConfig = {
     backgroundColor: 'lightgreen',
     fontColor: 'black',
     fontSize: '1.2vw',
-    width: '45%',
+    width: '47%',
+    onClick: onDetailsButtonClick,
   };
 
   return (
@@ -62,6 +70,7 @@ CustomerRow.propTypes = {
     email: PropTypes.string.isRequired,
     phone: PropTypes.string.isRequired,
     installments: PropTypes.arrayOf(PropTypes.shape({})),
+    _id: PropTypes.string.isRequired,
   }).isRequired,
 };
 
