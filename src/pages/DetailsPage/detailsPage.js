8570b6c367a474d98343f6591cc808d99ef0bb2f
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-06 16:10:57 -0300

    Added details page route

---
+++ src/pages/DetailsPage/detailsPage.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const DetailsPageSection = styled.section`
+
+`;
+
+const styles = {
+  DetailsPageSection,
+};
+
+export default styles;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import MainPage from './MainPage';
 import AddCustomerPage from './AddCustomerPage';
+import DetailsPage from './DetailsPage';
 
 export default {
   MainPage,
   AddCustomerPage,
+  DetailsPage,
 };
