c44b7f63c12b31636aed3457e4e399289e9e9f65
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-06 16:51:52 -0300

    Added all customer details to the page

--- src/pages/DetailsPage/detailsPage.js
+++ src/pages/DetailsPage/detailsPage.js
@@ -1,11 +1,43 @@
 import styled from 'styled-components';
 
 const DetailsPageSection = styled.section`
-
+  align-items: center;
+  background-color: #D1D1D1;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `;
+
+const CustomerInfoSection = styled.div`
+  align-items: center;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  margin: 5rem 0;
+  max-width: 100%;
+  padding: 12px;
+  width: 80%;
+`;
+
+const CustomerName = styled.h1`
+  font-size: 3vw;
+`;
+
+const CustomerInfo = styled.p`
+  font-size: 2vw;
+  margin: 2rem 0;
+`;
+
+const InstalmmentsContainer = styled.ul``;
 
 const styles = {
   DetailsPageSection,
+  CustomerInfoSection,
+  CustomerName,
+  CustomerInfo,
+  InstalmmentsContainer,
 };
 
 export default styles;
--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -3,12 +3,20 @@ import { useParams } from 'react-router-dom';
 import styles from './detailsPage';
 import Header from '../../components/Header';
 import CustomersAPI from '../../api/customersAPI';
+import getCurrencyFormat from '../../helpers/getCurrencyFormat';
 
 const { REACT_APP_API_URL } = process.env;
 const customersAPI = new CustomersAPI(REACT_APP_API_URL, 10000);
 
 function DetailsPage() {
-  const { DetailsPageSection } = styles;
+  const {
+    DetailsPageSection,
+    CustomerInfoSection,
+    CustomerName,
+    CustomerInfo,
+    InstalmmentsContainer,
+  } = styles;
+
   const { id } = useParams();
   const [customer, setCustomer] = useState({});
 
@@ -21,6 +29,35 @@ function DetailsPage() {
     getCustomer();
   }, []);
 
+  const formatInstallmentValue = (value) => getCurrencyFormat(value, 'pt-BR', 'currency', 'BRL');
+
+  const {
+    name,
+    email,
+    phone,
+    installments,
+  } = customer;
+
+  const formatMonth = (month) => {
+    if (month.toString().length === 1) {
+      return `0${month}`;
+    }
+
+    return month;
+  };
+
+  const getInstallments = (installment) => {
+    const date = new Date(installment.date);
+    const formattedDAte = `${date.getDate()}/${formatMonth(date.getMonth() + 1)}/${date.getFullYear()}`;
+
+    return (
+      <li>
+        <CustomerInfo>{ formattedDAte }</CustomerInfo>
+        <CustomerInfo>{ formatInstallmentValue(installment.value) }</CustomerInfo>
+      </li>
+    );
+  };
+
   return (
     <DetailsPageSection>
       <Header
@@ -28,6 +65,16 @@ function DetailsPage() {
         backButton
         backUrl="/"
       />
+      <CustomerInfoSection>
+        <CustomerName>{ name }</CustomerName>
+        <CustomerInfo>{ `Email: ${email}` }</CustomerInfo>
+        <CustomerInfo>{ `Telefone: ${phone}` }</CustomerInfo>
+        <InstalmmentsContainer>
+          {
+            installments.map((installment) => getInstallments(installment))
+          }
+        </InstalmmentsContainer>
+      </CustomerInfoSection>
     </DetailsPageSection>
   );
 }
