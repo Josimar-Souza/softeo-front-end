c1f41597de4d49347630259dff41dcfd7461a04c
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-09 15:29:39 -0300

    Created mock values for tests

---
+++ src/tests/mocks/customersMock.js
@@ -0,0 +1,175 @@
+const customersMock = [
+  {
+    name: 'Test 1',
+    email: 'test1@gmail.com',
+    phone: '21943625879',
+    installments: [
+      {
+        date: new Date('2022-05-15T00:00:00'),
+        value: 260,
+      },
+      {
+        date: new Date('2022-06-15T00:00:00'),
+        value: 260,
+      },
+      {
+        date: new Date('2022-07-15T00:00:00'),
+        value: 260,
+      },
+    ],
+  },
+  {
+    name: 'Test 2',
+    email: 'test2@gmail.com',
+    phone: '45996321578',
+    installments: [
+      {
+        date: new Date('2022-08-15T00:00:00'),
+        value: 160,
+      },
+      {
+        date: new Date('2022-09-15T00:00:00'),
+        value: 160,
+      },
+      {
+        date: new Date('2022-10-15T00:00:00'),
+        value: 160,
+      },
+      {
+        date: new Date('2022-11-15T00:00:00'),
+        value: 160,
+      },
+      {
+        date: new Date('2022-12-15T00:00:00'),
+        value: 160,
+      },
+      {
+        date: new Date('2023-01-15T00:00:00'),
+        value: 160,
+      },
+    ],
+  },
+  {
+    name: 'Test 3',
+    email: 'test3@gmail.com',
+    phone: '56932147898',
+    installments: [
+      {
+        date: new Date('2022-02-15T00:00:00'),
+        value: 340,
+      },
+      {
+        date: new Date('2022-03-15T00:00:00'),
+        value: 340,
+      },
+      {
+        date: new Date('2022-04-15T00:00:00'),
+        value: 340,
+      },
+      {
+        date: new Date('2022-05-15T00:00:00'),
+        value: 340,
+      },
+      {
+        date: new Date('2022-06-15T00:00:00'),
+        value: 340,
+      },
+    ],
+  },
+  {
+    name: 'Test 4',
+    email: 'test4@gmail.com',
+    phone: '21985632547',
+    installments: [
+      {
+        date: new Date('2022-04-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-05-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-06-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-07-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-08-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-09-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-10-15T00:00:00'),
+        value: 360,
+      },
+      {
+        date: new Date('2022-11-15T00:00:00'),
+        value: 360,
+      },
+    ],
+  },
+  {
+    name: 'Test 5',
+    email: 'test5@gmail.com',
+    phone: '78956214326',
+    installments: [
+      {
+        date: new Date('2022-07-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2022-08-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2022-09-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2022-10-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2022-11-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2022-12-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2023-01-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2023-02-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2023-03-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2023-04-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2023-05-15T00:00:00'),
+        value: 150,
+      },
+      {
+        date: new Date('2023-06-15T00:00:00'),
+        value: 150,
+      },
+    ],
+  },
+];
+
+export default customersMock;
