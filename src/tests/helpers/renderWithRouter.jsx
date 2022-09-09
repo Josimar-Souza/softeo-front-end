a69da9d549b1d70fe05cdb2c16d83583d440d7f6
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-09 15:34:18 -0300

    Created helper method renderWithRouter

---
+++ src/tests/helpers/renderWithRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+function renderWithRouter(component) {
+  return {
+    ...render(
+      <BrowserRouter>
+        { component }
+      </BrowserRouter>,
+    ),
+  };
+}
+
+export default renderWithRouter;
