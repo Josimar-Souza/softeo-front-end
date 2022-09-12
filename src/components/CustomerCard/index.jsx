637a1fb4747e66bd9b51b0c15a2b97d453a826bc
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-12 17:54:13 -0300

    Added button to CustomerCard component

--- src/components/CustomerCard/index.jsx
+++ src/components/CustomerCard/index.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './customerCardStyles';
+import phoneNumberFormatter from '../../helpers/phoneNumberFormatter';
+import getCurrencyFormat from '../../helpers/getCurrencyFormat';
+import Button from '../Button';
 
-function CustomerCard() {
-  const { CardStyle } = styles;
+function CustomerCard({ customer }) {
+  const {
+    CardStyle,
+    CustomerName,
+    CustomerInfo,
+    ActionsSection,
+  } = styles;
+
+  const getInstallmentTotal = (installments) => {
+    const total = installments.reduce((acc, curr) => acc + curr.value, 0);
+
+    return getCurrencyFormat(total, 'pt-BR', 'currency', 'BRL');
+  };
+
+  const removeButtonConfig = {
+    backgroundColor: 'red',
+    fontColor: 'white',
+    phoneFontSize: '4.5vw',
+    phoneWidth: '40%',
+  };
+
+  const detailsButtonConfig = {
+    backgroundColor: 'lightgreen',
+    fontColor: 'black',
+    phoneFontSize: '4.5vw',
+    phoneWidth: '40%',
+  };
+
   return (
     <CardStyle>
-      Card Style
+      <CustomerName>{ customer.name }</CustomerName>
+      <CustomerInfo>{`Email: ${customer.email}`}</CustomerInfo>
+      <CustomerInfo>{`Celular: ${phoneNumberFormatter(customer.phone)}`}</CustomerInfo>
+      <CustomerInfo>{`Total: ${getInstallmentTotal(customer.installments)}`}</CustomerInfo>
+      <ActionsSection>
+        <Button
+          config={removeButtonConfig}
+        >
+          Remover
+        </Button>
+        <Button
+          config={detailsButtonConfig}
+        >
+          Detalhes
+        </Button>
+      </ActionsSection>
     </CardStyle>
   );
 }
+
+CustomerCard.propTypes = {
+  customer: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    installments: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  }).isRequired,
+};
 
 export default CustomerCard;
